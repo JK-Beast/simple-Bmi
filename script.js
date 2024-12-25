e9bd0667b0d49d8fd65dec7af54edbb1742ed883
JK-Beast <147221162+JK-Beast@users.noreply.github.com>
2024-12-25 11:51:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ // Selecting the tags
+
+const cm=document.getElementById('height')
+const kg=document.getElementById('weight')
+
+const bmi=document.getElementById('rbmi')
+const r=document.getElementById('submit')
+const b=document.getElementById('body')
+
+function ans(){
+    console.log(cm)
+    console.log(kg)
+}
